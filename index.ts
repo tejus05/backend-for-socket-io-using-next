e70690a794f6f437649b042e1ee5fc78174f9c63
tejus <thequantumcoder@gmail.com>
2024-02-22 10:26:08 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express from "express"
+import cors from 'cors'
+import { Server } from "socket.io"
+import http from 'http'
+
+const app = express();
+
+app.use(cors);
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET","POST"]
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log(`${socket.id}`)
+  socket.on("sent-message", (message) => {
+    io.emit("received-message",message)
+  })
+})
+
+httpServer.listen(4000, () => {
+  console.log('listening on port 4000');
+})
